077599f132a0f1f3b6aa643140bb801b55b615cf
jupulgarh <jupulgar@unirioja.es>
2026-04-16 18:50:51 +0200

    basic tests

---
+++ proyecto/src/main/java/com/ejemplo/proyecto/domain/EntidadVirica.java
@@ -0,0 +1,7 @@
+package com.ejemplo.proyecto.domain;
+
+public class EntidadVirica extends Entidad {
+    public EntidadVirica(int x, int y) {
+        super(x, y);
+    }
+}
--- proyecto/src/main/java/com/ejemplo/proyecto/domain/EntidadVírica.java
+++
@@ -1,7 +0,0 @@
-package com.ejemplo.proyecto.domain;
-
-public class EntidadVírica extends Entidad {
-    public EntidadVírica(int x, int y) {
-        super(x, y);
-    }
-}
